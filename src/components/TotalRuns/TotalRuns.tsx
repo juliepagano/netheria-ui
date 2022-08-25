4f8bf3716c4917e405ca97624e55285aeed7dc26
Julie Pagano <julie.pagano@gmail.com>
2022-08-24 19:55:16 -0700

    refactor: renaming components for clarity and adding wrapper

---
+++ src/components/TotalRuns/TotalRuns.tsx
@@ -0,0 +1,8 @@
+const TotalRuns = () => {
+  return (
+    <section>
+      <h2>Total Runs</h2>
+    </section>
+  );
+};
+export default TotalRuns;
---
+++ src/components/TotalRuns/index.ts
@@ -0,0 +1,2 @@
+export { default } from "./TotalRuns";
+export * from "./TotalRuns";
